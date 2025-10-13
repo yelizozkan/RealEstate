3c068a97350f51f3157c8a1b36a6a57b81f301dd
Yeliz Özkan <yelizozkan337@gmail.com>
2025-10-14 01:56:04 +0300

    Add project files.

---
+++ RealEstate.Domain/Entities/Listing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstate.Domain.Common;
+
+namespace RealEstate.Domain.Entities
+{
+    public class Listing : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public decimal? Price { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public double? Area { get; set; }
+        public int? Rooms { get; set; }
+        public bool IsPublished { get; set; } = true;
+
+        public ICollection<Photo> Photos { get; set; } = new List<Photo>();
+    }
+}
---
+++ RealEstate.Domain/Entities/Photo.cs
@@ -0,0 +1,19 @@
+﻿using RealEstate.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Domain.Entities
+{
+    public class Photo : BaseEntity
+    {
+        public string FileName { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+
+        public Guid ListingId { get; set; }
+        public Listing Listing { get; set; }
+    }
+}
